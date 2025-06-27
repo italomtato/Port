28e04bd966a06faf4611d7a70c64fbbacaf04547
italomtato <italo.marques.tato@gmail.com>
2025-06-26 22:36:22 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -137,7 +137,7 @@ const Home: React.FC = () => {
               </div>
               <div className="flex space-x-4 mt-2">
                 <a
-                  href={socialLinks.github}
+                  href="https://github.com/ItaloHenriqueMarquesCampos"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="p-2 bg-gray-100 dark:bg-gray-700 rounded-full text-gray-600 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-900 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
@@ -147,7 +147,7 @@ const Home: React.FC = () => {
                   </svg>
                 </a>
                 <a
-                  href={socialLinks.linkedin}
+                  href="https://www.linkedin.com/in/data-italomarques/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="p-2 bg-gray-100 dark:bg-gray-700 rounded-full text-gray-600 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-900 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
@@ -157,7 +157,7 @@ const Home: React.FC = () => {
                   </svg>
                 </a>
                 <a
-                  href={socialLinks.facebook}
+                  href="https://www.facebook.com/italo.marques.tato"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="p-2 bg-gray-100 dark:bg-gray-700 rounded-full text-gray-600 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-900 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
@@ -167,17 +167,7 @@ const Home: React.FC = () => {
                   </svg>
                 </a>
                 <a
-                  href={socialLinks.twitter}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="p-2 bg-gray-100 dark:bg-gray-700 rounded-full text-gray-600 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-900 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
-                >
-                  <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
-                    <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
-                  </svg>
-                </a>
-                <a
-                  href={socialLinks.instagram}
+                  href="https://www.instagram.com/data.italomarques/"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="p-2 bg-gray-100 dark:bg-gray-700 rounded-full text-gray-600 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-blue-900 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-300"
