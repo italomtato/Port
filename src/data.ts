012a116f90637ef906a6092a894c4d05d59ec8a5
italomtato <italo.marques.tato@gmail.com>
2025-06-27 00:59:43 -0300

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -16,6 +16,14 @@ export const initialProjects: Project[] = [
     image: "/planilhaconsorcio.jpeg",
     tags: ["Google Sheets"],
     link: "#"
+  },
+    {
+    id: 3,
+    title: "Planilha de Comissionamento de Vendas – Consórcio",
+    description: "Desenvolvimento de uma planilha estratégica para controle de comissões, integrando dados de vendedores, gestores e empresa. A solução permite acompanhar metas, calcular comissões de forma automatizada e gerar relatórios mensais organizados.",
+    image: "/planilhaconsorcio.jpeg",
+    tags: ["Google Sheets"],
+    link: "#"
   }
 ];
 
